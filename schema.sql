17e7b274e84bfaade26966dfde6c608f6d363293
Hayder Thamer <hayder.thamer.mahdi@gmail.com>
2025-11-18 00:55:56 +0300

    Fix dublicate indexes errors

--- schema.sql
+++ schema.sql
@@ -99,7 +99,6 @@ CREATE TABLE public.staff (
 );
 ALTER TABLE public.staff ENABLE ROW LEVEL SECURITY;
 -- #endregion [staff]
-
 -- #region [project_staff_financial_fees]
 DROP TABLE IF EXISTS public.project_staff_financial_fees CASCADE;
 CREATE TABLE public.project_staff_financial_fees (
@@ -111,7 +110,6 @@ CREATE TABLE public.project_staff_financial_fees (
 );
 ALTER TABLE public.project_staff_financial_fees ENABLE ROW LEVEL SECURITY;
 -- #endregion [project_staff_financial_fees]
-
 -- #region [project_staff_financial_fees_fixed]
 DROP TABLE IF EXISTS public.project_staff_financial_fees_fixed CASCADE;
 CREATE TABLE public.project_staff_financial_fees_fixed (
@@ -120,30 +118,28 @@ CREATE TABLE public.project_staff_financial_fees_fixed (
   title TEXT NOT NULL,
   amount BIGINT NOT NULL,
   notes TEXT,
-  CONSTRAINT cash_amount_check CHECK (amount > 0),
-  CONSTRAINT project_staff_financial_fees_fixed_project_staff_financial_fees_id_title_key UNIQUE (project_staff_financial_fees_id, title)
+  CONSTRAINT psfff_amount_check CHECK (amount > 0),
+  CONSTRAINT psfff_project_staff_financial_fees_id_title_key UNIQUE (project_staff_financial_fees_id, title)
 );
 ALTER TABLE public.project_staff_financial_fees_fixed ENABLE ROW LEVEL SECURITY;
-CREATE INDEX project_staff_financial_fees_fixed_project_staff_financial_fees_id_idx ON public.project_staff_financial_fees_fixed (project_staff_financial_fees_id);
+CREATE INDEX psfff_project_staff_financial_fees_id_idx ON public.project_staff_financial_fees_fixed (project_staff_financial_fees_id);
 -- #endregion [project_staff_financial_fees_fixed]
-
 -- #region [project_staff_financial_fees_items]
 DROP TABLE IF EXISTS public.project_staff_financial_fees_items CASCADE;
 CREATE TABLE public.project_staff_financial_fees_items (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
   project_staff_financial_fees_id UUID NOT NULL REFERENCES public.project_staff_financial_fees (id) ON DELETE CASCADE ON UPDATE CASCADE,
   title TEXT NOT NULL,
-  quantity NUMERIC NOT NULL CHECK (quantity > 0),
-  unit_price BIGINT NOT NULL CHECK (unit_price > 0),
+  quantity NUMERIC NOT NULL,
+  unit_price BIGINT NOT NULL,
   notes TEXT,
-  CONSTRAINT project_staff_financial_fees_items_project_staff_financial_fees_id_title_key UNIQUE (project_staff_financial_fees_id, title),
-  CONSTRAINT project_staff_financial_fees_items_quantity_check CHECK (quantity > 0),
-  CONSTRAINT project_staff_financial_fees_items_unit_price_check CHECK (unit_price > 0)
+  CONSTRAINT psffi_project_staff_financial_fees_id_title_key UNIQUE (project_staff_financial_fees_id, title),
+  CONSTRAINT psffi_quantity_check CHECK (quantity > 0),
+  CONSTRAINT psffi_unit_price_check CHECK (unit_price > 0)
 );
 ALTER TABLE public.project_staff_financial_fees_items ENABLE ROW LEVEL SECURITY;
-CREATE INDEX project_staff_financial_fees_items_project_staff_financial_fees_id_idx ON public.project_staff_financial_fees_items (project_staff_financial_fees_id);
+CREATE INDEX psffi_project_staff_financial_fees_id_idx ON public.project_staff_financial_fees_items (project_staff_financial_fees_id);
 -- #endregion [Tables]
-
 -- #endregion [project_staff_financial_fees_items]
 -- ############################################################
 
